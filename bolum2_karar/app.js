e1f92ca511d29c71dc0e5b0dc5a840b6467ca947
ebru77 <76748372+ebru77@users.noreply.github.com>
2021-06-11 18:55:46 +0200

    javaScriptOdevler olusturuldu

--- bolum2_karar/app.js
+++ bolum2_karar/app.js
@@ -11,7 +11,7 @@ sonuc = "cocuk"
 }
 console.log(sonuc); yetiskin yazdirir*/
 
-const s1 = Number(prompt("Birinci sayi :"));
+/* const s1 = Number(prompt("Birinci sayi :"));
 const islem = prompt("islemi giriniz");
 const s2 = Number(prompt("Ikinci sayi :"));
 let sonuc1 =0;
@@ -30,4 +30,46 @@ console.log(`${s1}${islem}${s2} = ${sonuc1}`);
 
 const age = prompt("yasinizi giriniz");
 const healt = prompt("saglikli misiniz e/h");
-alert(age>18 && healt=="e" ? "Askere gidebilir" : "Askere gidemez")
+alert(age>18 && healt=="e" ? "Askere gidebilir" : "Askere gidemez")*/
+
+
+/*const gunNo = Number(prompt("Gun sayisini giriniz:"));
+switch (gunNo) {
+ case 1:
+  gunAdi = "Pazartesi";
+  break;
+  case 2:
+  gunAdi = "Sali";
+  break;
+  case 3:
+  gunAdi = "Carsamba";
+  break;
+  case 4:
+  gunAdi = "Persembe";
+  break;
+  case 5:
+  gunAdi = "Cuma";
+  break;
+  case 6:
+  gunAdi = "Cumartesi";
+  break;
+  case 7:
+  gunAdi = "Pazar";
+  break;
+ default:
+  alert("Gun 1-7 arasinda olmalidir");
+  break;
+}
+console.log(gunAdi);*/
+
+/*const maas = prompt("Maasinizi giriniz:");
+const calismaSuresi = prompt("Calisma suresini giriniz");
+const zamliMaas = calismaSuresi>10 ? maas*1.5 :calismaSuresi>5 ? maas*1.2 : maas*1.1
+console.log(zamliMaas);*/
+
+const borc = 0;
+const maas = 3000;
+console.log(!borc && maas>2825.9 ? "Kredi alabilir 🤑 " : "Kredi alamaz 🥺");
+
+
+
